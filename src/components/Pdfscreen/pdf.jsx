68f60c4fee5ecc2f79cdd5db8da2029b24883a59
Hariharantrugo <hariharan.t@trugotech.com>
2025-03-27 15:39:14 +0530

    pdf alignment change

--- src/components/Pdfscreen/pdf.jsx
+++ src/components/Pdfscreen/pdf.jsx
@@ -2,9 +2,11 @@ import React, { useRef } from "react";
 import jsPDF from "jspdf";
 import html2canvas from "html2canvas";
 import "bootstrap/dist/css/bootstrap.min.css";
+import { useReactToPrint } from "react-to-print";
 
 const PdfScreen = () => {
     const contentRef = useRef(null);
+    const reactToPrintFn = useReactToPrint({ contentRef });
 
     const generatePDF = () => {
         const input = contentRef.current;
@@ -33,25 +35,26 @@ const PdfScreen = () => {
                 </div>
 
                 {/* Main Content Section */}
-                <div className="p-4" style={{ flex: 1,fontSize:'14px', textAlign: "justify" ,marginTop:"30px",marginLeft:'30px',marginRight:"30px"}}>
+                <div className="p-4 " >
                     <h5 className="text-center fw-bold text-dark">அறிவிப்பு:</h5>
-                    <p className="text-dark  " style={{marginTop:'40px'}}>
-                        சேலம் மாவட்டம், மேட்டூர் தொகுதி, <strong>207</strong>ஆவது வாக்குச்சாவடியில்
-                        <strong> சிவானந்தம் வீரபாண்டியன் (18574358150) </strong>, நாம் தமிழர் கட்சி - இரணியூர் பாசறையின் மாபெரும் ஒருங்கிணைப்பாளராக
-                        ஒருமனதாக நியமிக்கப்படுகிறார்.
-                    </p>
-                    <p className="text-dark">
-                        இதற்கு, கட்சியின் அனைத்து உறுப்பினர்களும், மாவட்ட மற்றும் ஒன்றிய முழு ஒருங்கிணைப்பாளர்களும் ஒத்துழைப்பு வழங்க வேண்டும் என்று கேட்டுக் கொள்கிறோம்.
-                    </p>
-                    <p className="text-dark">
-                        இது தொடர் வெற்றியை நோக்கி நாம் தமிழர் கட்சி முன்னேற தொடர்ந்த பயணத்திற்கான உறுதிப்பாட்டாகும்.
-                    </p>
-                    <p className="text-dark">
-                        கட்சியின் அனைத்து உறுப்பினர்களும், ஆதரவாளர்களும், பொறுப்பாளர்களும் நம்பிக்கையுடன் செயல்பட வேண்டும்.
-                    </p>
-
+                    <div className="desContent2 d-flex flex-column p-2">
+                        <p className="text-dark  " >
+                            சேலம் மாவட்டம், மேட்டூர் தொகுதி, <strong>207</strong>ஆவது வாக்குச்சாவடியில்
+                            <strong> சிவானந்தம் வீரபாண்டியன் (18574358150) </strong>, நாம் தமிழர் கட்சி - இரணியூர் பாசறையின் மாபெரும் ஒருங்கிணைப்பாளராக
+                            ஒருமனதாக நியமிக்கப்படுகிறார்.
+                        </p>
+                        <p className="text-dark">
+                            இதற்கு, கட்சியின் அனைத்து உறுப்பினர்களும், மாவட்ட மற்றும் ஒன்றிய முழு ஒருங்கிணைப்பாளர்களும் ஒத்துழைப்பு வழங்க வேண்டும் என்று கேட்டுக் கொள்கிறோம்.
+                        </p>
+                        <p className="text-dark">
+                            இது தொடர் வெற்றியை நோக்கி நாம் தமிழர் கட்சி முன்னேற தொடர்ந்த பயணத்திற்கான உறுதிப்பாட்டாகும்.
+                        </p>
+                        <p className="text-dark">
+                            கட்சியின் அனைத்து உறுப்பினர்களும், ஆதரவாளர்களும், பொறுப்பாளர்களும் நம்பிக்கையுடன் செயல்பட வேண்டும்.
+                        </p>
+                    </div>
                     {/* Signature Section */}
-                    <div className="text-end">
+                    <div className="text-end" style={{marginRight:"20px"}}>
                         <p className="fw-semibold">சீமான்</p>
                         <p className="text-muted">தலைமை ஒருங்கிணைப்பாளர்</p>
                     </div>
@@ -65,7 +68,7 @@ const PdfScreen = () => {
 
             {/* PDF Download Button */}
             <div className="text-center mt-3">
-                <button onClick={generatePDF} className="btn btn-primary">
+                <button onClick={() => reactToPrintFn()} className="btn btn-primary">
                     PDF பதிவிறக்க
                 </button>
             </div>
